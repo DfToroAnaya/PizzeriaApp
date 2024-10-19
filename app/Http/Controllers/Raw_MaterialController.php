9cbe23d6447c67ceea279dd647b9ab7d4e096910
mistercristian <cristianguzbo@gmail.com>
2024-10-19 17:15:43 -0500

    Controllers

--- app/Http/Controllers/Raw_MaterialController.php
+++ app/Http/Controllers/Raw_MaterialController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class IngredientesController extends Controller
+class Raw_MaterialController extends Controller
 {
     /**
      * Display a listing of the resource.
