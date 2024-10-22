e2f1aca71bf1483f95b4770f5bb2a56de4c2d499
Kevin-SantiagoC <santiagocastano47@gmail.com>
2024-10-21 23:14:01 -0500

    Employees

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -52,6 +52,12 @@
                     </x-nav-link>
                 </div>
 
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('employees.index')" :active="request()->routeIs('employees.index')">
+                        {{ __('Employees') }}
+                    </x-nav-link>
+                </div>
+
             </div>
 
             <!-- Settings Dropdown -->
@@ -127,6 +133,14 @@
 
             <x-responsive-nav-link :href="route('branches.index')" :active="request()->routeIs('branches.index')">
                 {{ __('Branches') }}
+            </x-responsive-nav-link>
+
+            
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('employees.index')" :active="request()->routeIs('employees.index')">
+                {{ __('Employees') }}
             </x-responsive-nav-link>
         </div>
 
