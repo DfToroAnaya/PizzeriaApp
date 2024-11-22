bc98db2386b002adbff2ae9605688a3b6f50fbe5
Juan Gomez <juangomez130703@gmail.com>
2024-11-22 01:17:34 -0500

    Order_Pizzas API

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\ClientController;
 use App\Http\Controllers\api\OrderController;
+use App\Http\Controllers\api\Order_PizzaController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -25,5 +26,11 @@ Route::delete('/orders/{order}', [OrderController::class, 'destroy'])->name('ord
 Route::put('/orders/{order}', [OrderController::class, 'update'])->name('orders.update');
 Route::get('/orders/{order}/edit', [OrderController::class, 'edit'])->name('orders.edit');
 
-
+//ROUTES ORDER_PIZZAS
+Route::get('/order_pizzas', [Order_PizzaController::class, 'index'])->name('order_pizzas');
+Route::post('/order_pizzas', [Order_PizzaController::class, 'store'])->name('order_pizzas.store');
+Route::get('/order_pizzas/create', [Order_PizzaController::class, 'create'])->name('order_pizzas.create');
+Route::delete('/order_pizzas/{order_pizza}', [Order_PizzaController::class, 'destroy'])->name('order_pizzas.destroy');
+Route::put('/order_pizzas/{order_pizza}', [Order_PizzaController::class, 'update'])->name('order_pizzas.update');
+Route::get('/order_pizzas/{order_pizza}/edit', [Order_PizzaController::class, 'edit'])->name('order_pizzas.edit');
 
