b132df1e14a719723575b1241d35c3a522e6a966
Juan Gomez <juangomez130703@gmail.com>
2024-11-22 01:05:44 -0500

    Orders API

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,27 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\ClientController;
+use App\Http\Controllers\api\OrderController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+//ROUTES CLIENTS
 Route::get('/clients', [ClientController::class, 'index'])->name('clients');
 Route::post('/clients', [ClientController::class, 'store'])->name('clients.store');
 Route::get('/clients/create', [ClientController::class, 'create'])->name('clients.create');
 Route::delete('/clients/{client}', [ClientController::class, 'destroy'])->name('clients.destroy');
 Route::put('/clients/{client}', [ClientController::class, 'update'])->name('clients.update');
 Route::get('/clients/{client}/edit', [ClientController::class, 'edit'])->name('clients.edit');
+
+//ROUTES ORDERS
+Route::get('/orders', [OrderController::class, 'index'])->name('orders');
+Route::post('/orders', [OrderController::class, 'store'])->name('orders.store');
+Route::get('/orders/create', [OrderController::class, 'create'])->name('orders.create');
+Route::delete('/orders/{order}', [OrderController::class, 'destroy'])->name('orders.destroy');
+Route::put('/orders/{order}', [OrderController::class, 'update'])->name('orders.update');
+Route::get('/orders/{order}/edit', [OrderController::class, 'edit'])->name('orders.edit');
+
+
+
